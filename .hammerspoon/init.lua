9b5820ff566249bdc8b22d3c28e732984c9fc854
Artem Hrytsaienko <artemhrytsaienko@Artems-MacBook-Pro.local>
2020-05-17 20:19:50 +0300

    hamerspoon

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,11 @@
+hs.hotkey.bind({}, "F16", function()
+    hs.keycodes.setLayout("U.S.")
+end)
+
+hs.hotkey.bind({}, "F17", function()
+    hs.keycodes.setLayout("Russian - PC")
+end)
+
+hs.hotkey.bind({}, "F18", function()
+    hs.keycodes.setLayout("Ukrainian - PC")
+end)
